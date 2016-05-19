3f303bc388f09fbff1a024815c6d6357abece0cb
Romário Lima <me@romar.io>
2016-05-18 22:06:02 -0300

    chore(express): adiciona estrutura básica

--- src/server/server.js
+++ src/server/server.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+import Express from 'express';
+import http from 'http';
+import morgan from 'morgan';
+
+const app = new Express();
+const server = new http.Server(app);
+
+const { PORT: port, NODE_PATH: path } = process.env;
+
+app.use(morgan('dev'));
+
+app.use('/assets', Express.static(`${path}/../assets`));
+
+server.listen(port, () => {
+  const host = server.address().address;
+  console.log(`♥ Spreading Cleytoness in http://${host}:${port} ♥`);
+});
